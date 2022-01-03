665ee29db8e52a3d0687e720ebe6e1c607c15b66
Nicolas F <96670232+nf4928394@users.noreply.github.com>
2022-01-03 15:35:12 -0600

    Updated numToRoman function (completed)

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
 # Function that converts a Roman numeral to a regular number.
-# COMPLETED
+# Status: COMPLETED
 def romanToNum(str):
 	map = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000} # Hash map used to calculate numerical values for Roman letters.
 	val = 0 # Used to store the ongoing total.
@@ -11,27 +11,25 @@ def romanToNum(str):
 	return val
 
 # Function that converts a number to a Roman numeral. 
-# IN PROGRESS. Function needs to be completed and tested before being marked complete.
+# Status: COMPLETED
 def numToRoman(int):
 	roman = ""
-	map_ones = {1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX"}
-	map_tens = {1:"X", 2:"XX", 3:"XXX", 4:"XL", 5:"L", 6:"LX", 7:"LXX", 8:"LXXX", 9:"XC"}
-	map_hunds = {1:"C", 2:"CC", 3:"CCC", 4:"CD", 5:"D", 6:"DC", 7:"DCC", 8:"DCCC",9:"CM"}
-	map_thous = {1:"M", 2:"MM", 3:"MMM"}
+	map2 = {1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX", 10:"X", 20:"XX", 30:"XXX", 40:"XL", 50:"L", 60:"LX", 70:"LXX", 80:"LXXX", 90:"XC",
+	100:"C", 200:"CC", 300:"CCC", 400:"CD", 500:"D", 600:"DC", 700:"DCC", 800:"DCCC", 900:"CM", 1000:"M", 2000:"MM", 3000:"MMM"}
+	tenslist = [1, 10, 100, 1000]
+	numlist = []
 
-	placelist = ("ones", "tens", "hunds", "thous")
-	numdict = {}
-	int_hold = int
- 	for i in placelist: # Breaks int_hold into separate values for ones, tens, hundreds, and thousands place. Stores in numdict.
-		numdict[i] = int_hold % 10
-		int_hold //= int_hold
+	for i in tenslist: # Breaks int_hold into separate values for ones, tens, hundreds, and thousands place. Stores in numdict.
+		numlist.append(int % 10 * i)
+		int //= 10
 
-	for j in placelist: # Appends Roman numerals to the string 'roman' based on the values stored in numdict and the four hash maps above.
-        if numdict[j] != 0:
-        	roman = numdict[j] + roman
+	for j in numlist: # Appends Roman numerals to the string 'roman' based on the values stored in numdict and the four hash maps above.
+		if j != 0:
+			roman = map2[j] + roman
+	return roman
 
 # Function that receives user input, performs error control, calls one of the two functions above, and prints out the returned conversion.  
-# IN PROGRESS. Function needs to be completed and tested before being marked complete.
+# Status: IN PROGRESS; function needs to be completed and tested before being marked complete.
 def receiveUserInput():
 	user_input = input("Please enter your Roman numeral or number here: ")
 	try:
